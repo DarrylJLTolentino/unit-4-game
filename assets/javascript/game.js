4433292e61409e3f66867cc1c2d0e1364f443ef9
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-05-27 14:39:21 -0700

    Changing name of subzero's image and including art to objects within the game.js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,27 @@
 var scorpion = {
     hp: 125,
     power: 10, 
-    counterPower: 11
+    counterPower: 11,
+    art: "../images/scorpion.png"
 }
 
-var subzero = {
+var sub_zero = {
     hp: 130,
     power: 9,
-    counterPower: 12
+    counterPower: 12,
+    art: "../images/sub_zero.png"
 }
 
 var noobsaitbot = {
     hp: 140,
     power: 8,
-    counterPower: 13
+    counterPower: 13,
+    art: "../images/noobsaibot.png"
 }
 
 var smoke = {
     hp: 150,
     power: 7,
-    counterPower: 14
+    counterPower: 14,
+    art: "../images/smoke.png"
 }
