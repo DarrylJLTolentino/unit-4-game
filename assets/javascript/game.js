18b0608a46245276e0ae076c57fbc9c4b2535f33
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-05-27 15:33:35 -0700

    Adding a for loop that creates character select divisions based on objects in an array in game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -36,41 +36,43 @@ var charArray = [
         hp: 125,
         power: 10,
         counterPower: 11,
-        art: "../images/scorpion.png"
+        art: "assets/images/scorpion.png"
     },
     sub_zero = {
         name: "Sub-Zero",
         hp: 130,
         power: 9,
         counterPower: 12,
-        art: "../images/sub_zero.png"
+        art: "assets/images/sub_zero.png"
     },
     noobsaitbot = {
         name: "Noob Saibot",
         hp: 140,
         power: 8,
         counterPower: 13,
-        art: "../images/noobsaibot.png"
+        art: "assets/images/noobsaibot.png"
     },
     smoke = {
         name: "Smoke",
         hp: 150,
         power: 7,
         counterPower: 14,
-        art: "../images/smoke.png"
+        art: "assets/images/smoke.png"
     }];
 
-// var charSelect = $(".charSelect");
+var charSelect = $(".charSelect");
 
-// for (var i = 0; i < charArray.length; i++) {
-//     var newCharDiv = $("<a>");
-//     newCharDiv.attr({
-//         "name": charArray[i].name,
-//         "hp": charArray[i].hp,
-//         "power": charArray[i].power,
-//         "counterPower": charArray[i].counterPower,
-//         "src": charArray[i].art
-//     });
-//     newCharDiv.innerHTML = charArray[i];
-//     charSelect.append(newCharDiv);
+for (var i = 0; i < charArray.length; i++) {
+    var newCharDiv = $("<div>");
+    var newCharImg = $("<img>");
+    newCharDiv.attr({
+        "name": charArray[i].name,
+        "hp": charArray[i].hp,
+        "power": charArray[i].power,
+        "counterPower": charArray[i].counterPower,
+    });
+    newCharImg.attr({"src": charArray[i].art});
+    newCharDiv.appendChild = newCharImg;
+    newCharDiv.innerHTML = charArray[i];
+    charSelect.append(newCharDiv);
 }
