0ada85262136913a9d1b9594182b19199a3df94c
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-05-27 15:17:00 -0700

    Put objects within an array in order to call using a for loop within game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,76 @@
-var scorpion = {
-    name: "Scorpion",
-    hp: 125,
-    power: 10, 
-    counterPower: 11,
-    art: "../images/scorpion.png"
-}
+// var scorpion = {
+//     name: "Scorpion",
+//     hp: 125,
+//     power: 10,
+//     counterPower: 11,
+//     art: "../images/scorpion.png"
+// }
 
-var sub_zero = {
-    name: "Sub-Zero",
-    hp: 130,
-    power: 9,
-    counterPower: 12,
-    art: "../images/sub_zero.png"
-}
+// var sub_zero = {
+//     name: "Sub-Zero",
+//     hp: 130,
+//     power: 9,
+//     counterPower: 12,
+//     art: "../images/sub_zero.png"
+// }
 
-var noobsaitbot = {
-    name: "Noob Saibot",
-    hp: 140,
-    power: 8,
-    counterPower: 13,
-    art: "../images/noobsaibot.png"
-}
+// var noobsaitbot = {
+//     name: "Noob Saibot",
+//     hp: 140,
+//     power: 8,
+//     counterPower: 13,
+//     art: "../images/noobsaibot.png"
+// }
 
-var smoke = {
-    name: "Smoke",
-    hp: 150,
-    power: 7,
-    counterPower: 14,
-    art: "../images/smoke.png"
-}
+// var smoke = {
+//     name: "Smoke",
+//     hp: 150,
+//     power: 7,
+//     counterPower: 14,
+//     art: "../images/smoke.png"
+// }
 
-var charArray = [scorpion, sub_zero, noobsaitbot, smoke]
+var charArray = [
+    scorpion = {
+        name: "Scorpion",
+        hp: 125,
+        power: 10,
+        counterPower: 11,
+        art: "../images/scorpion.png"
+    },
+    sub_zero = {
+        name: "Sub-Zero",
+        hp: 130,
+        power: 9,
+        counterPower: 12,
+        art: "../images/sub_zero.png"
+    },
+    noobsaitbot = {
+        name: "Noob Saibot",
+        hp: 140,
+        power: 8,
+        counterPower: 13,
+        art: "../images/noobsaibot.png"
+    },
+    smoke = {
+        name: "Smoke",
+        hp: 150,
+        power: 7,
+        counterPower: 14,
+        art: "../images/smoke.png"
+    }];
 
-var charSelect = $("#charSelect");
-for(var i = 0; i < charArray.length; i++) {
-    var newCharDiv = $("<div>").charArray[i];
-    charSelect.append(newCharDiv);
+// var charSelect = $(".charSelect");
+
+// for (var i = 0; i < charArray.length; i++) {
+//     var newCharDiv = $("<a>");
+//     newCharDiv.attr({
+//         "name": charArray[i].name,
+//         "hp": charArray[i].hp,
+//         "power": charArray[i].power,
+//         "counterPower": charArray[i].counterPower,
+//         "src": charArray[i].art
+//     });
+//     newCharDiv.innerHTML = charArray[i];
+//     charSelect.append(newCharDiv);
 }
