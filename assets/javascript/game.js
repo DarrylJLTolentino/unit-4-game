7da852335346c71fc5887fd9350dcbb4e3adbdf0
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-05-27 14:15:41 -0700

    Adding character objects for use within game in game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+var scorpion = {
+    hp: 125,
+    power: 10, 
+    counterPower: 11
+}
+
+var subzero = {
+    hp: 130,
+    power: 9,
+    counterPower: 12
+}
+
+var noobsaitbot = {
+    hp: 140,
+    power: 8,
+    counterPower: 13
+}
+
+var smoke = {
+    hp: 150,
+    power: 7,
+    counterPower: 14
+}
